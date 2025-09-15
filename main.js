4084c75f990649ffda9a105590073619e22daa92
Luiz Gabriel <120055985+Gabsz193@users.noreply.github.com>
2025-09-15 07:59:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3010, () => {
-    console.log("Server running at http://localhost:3010/");
+    console.log("Server running on http://localhost:3010/");
 });
